fb0430e7302febca3d6a06435f4361d91822267f
Daniel Beardsley <daniel@ifixit.com>
2012-06-06 19:35:12 -0700

    Make feature finish submit a pull-request

--- github.rb
+++ github.rb
@@ -1,13 +1,21 @@
 require 'rubygems'
 require 'octokit'
 require 'readline'
+require 'shellwords'
 
 module Github
    ##
    # Get a global git config property
    ##
    def self.config(property)
-      `git config --global github.#{property.shellescape}`.strip
+      `git config --global github.#{property.to_s.shellescape}`.strip
+   end
+
+   ##
+   # Get a local (to the repo) git config property
+   ##
+   def self.local_config(property)
+      `git config github.#{property.to_s.shellescape}`.strip
    end
 
    ##
@@ -16,14 +24,20 @@ module Github
    # Authorization info is the structure from here:
    # http://developer.github.com/v3/oauth/#create-a-new-authorization
    #
-   # like this:
+   # something like this:
    # {
    #     :scopes => ['repo'],
-   #     :note => "ifixit git-scripts command line interface",
+   #     :note => "git-scripts command line interface",
    #     :note_url => "https://github.com/ifixit/git-scripts"
    # }
    ##
-   def self.api(authorization_info)
+   def self.api(authorization_info = {})
+      # Defaults
+      authorization_info = {
+         :scopes => ['repo'],
+         :note => "ifixit git-scripts command line interface",
+         :note_url => "https://github.com/ifixit/git-scripts"
+      }.merge(authorization_info)
       Octokit::Client.new(self::get_authentication(authorization_info))
    end
 
@@ -67,6 +81,30 @@ module Github
       end
    end
 
+   ##
+   # Returns the github repo identifier in the form that the API likes:
+   # "someuser/theirrepo"
+   #
+   # Prompts the user for this value if it isn't in git config
+   ##
+   def self.get_github_repo()
+      while ((repo = self::local_config("repo")).to_s.empty?) do
+         repo = Readline.readline("Provide a github repo: (like: someuser/repo_name): ", true)
+         if (repo =~ /\w+\/\w+/)
+            system("git config github.repo #{repo.shellescape}")
+         else
+            puts "Repo must be in the format: someuser/repo_name"
+         end
+      end
+      repo
+   end
+
+   ##
+   # Prompts the user (using $EDITOR) to provide a title and body
+   # for this pull-request
+   #
+   # Returns a hash containing a :title and :body
+   ##
    def self.get_pull_request_description()
       require 'tempfile'
       msg = Tempfile.new('pull-message')
